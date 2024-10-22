4a2f7dd3a9d45cef500dbf2fd60a7ec25b351341
MathiMat512 <mathias.mathimat12@gmail.com>
2024-10-22 01:14:19 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { ContactosComponent } from './contactos/contactos.component';
+import { ImagenesComponent } from './imagenes/imagenes.component';
+import { DetallesComponent } from './detalles/detalles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'quienes-somos', component: QuienesSomosComponent },
+  { path: 'contactos', component: ContactosComponent },
+  { path: 'imagenes', component: ImagenesComponent },
+  { path: 'detalles', component: DetallesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { ContactosComponent } from './contactos/contactos.component';
+import { ImagenesComponent } from './imagenes/imagenes.component';
+import { DetallesComponent } from './detalles/detalles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuienesSomosComponent,
+    ContactosComponent,
+    ImagenesComponent,
+    DetallesComponent
   ],
   imports: [
     BrowserModule,
